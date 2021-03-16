7cf8a1fb06393ada34829c38a5604a705fe42be3
tsengyiching <yictseng@student.42lyon.fr>
2021-03-16 11:31:25 +0100

    norminettev2

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yictseng <yictseng@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 16:44:21 by yictseng          #+#    #+#             */
-/*   Updated: 2021/03/15 17:29:59 by yictseng         ###   ########lyon.fr   */
+/*   Updated: 2021/03/16 11:30:52 by yictseng         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,19 @@
 #  define BUFFER_SIZE 1
 # endif
 
-typedef struct		s_stock
+typedef struct s_stock
 {
-	char			*str;
-	int				eof;
-}					t_stock;
+	char	*str;
+	int		eof;
+}			t_stock;
 
-size_t				ft_strlen(const char *s);
-int					ft_strchr(const char *s, int c);
-int					get_next_line(int fd, char **line);
-char				*ft_substr(char const *s, unsigned int start, size_t len);
-char				*ft_strjoin(char *s1, char const *s2);
-int					ft_atoi(const char *str);
-int					ft_isdigit(int c);
-void				ft_putstr_fd(char *s, int fd);
-int					ft_strcmp(const char *s1, const char *s2);
+size_t		ft_strlen(const char *s);
+int			ft_strchr(const char *s, int c);
+int			get_next_line(int fd, char **line);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char *s1, char const *s2);
+int			ft_atoi(const char *str);
+int			ft_isdigit(int c);
+void		ft_putstr_fd(char *s, int fd);
+int			ft_strcmp(const char *s1, const char *s2);
 #endif
